c051bc7e59d8276e95fd96d21b2bcf3b69cb4367
Mohammed Faysal <faysalrj361@gmail.com>
2021-08-27 15:27:03 +0600

    dynamic added

--- js/countries.js
+++ js/countries.js
@@ -9,12 +9,29 @@ const displayCountries = (countries) => {
     const divContainer = document.getElementById('countries');
     countries.forEach(country => {
         const div = document.createElement('div');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        div.classList.add('country');
+        div.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name}')">Details</button>
+        `
         divContainer.appendChild(div);
+
     })
 }
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayCountryDetail(data[0]));
+ }
+
+ const displayCountryDetail = country => {
+    const divContainer2 = document.getElementById('country-detail');
+    divContainer2.innerHTML = `
+    <h3>Country : ${country.name}</h3>
+    <p>Population : ${country.population}</p>
+    <img width="200px" src="${country.flag}">
+    `
+ }
